79c09ed564b2cd65ca907e825ea68a93f031ad6c
Eduardo <el_alienx@hotmail.com>
2022-03-21 09:23:25 +0100

    added the code from jsonplaceholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+// NPM packages
+import { useEffect, useState } from "react";
+
 export default function App() {
+  // Local state
+  const [task, setTask] = useState({});
+
+  // Method
+  // 1.The arrow function
+  // 2. Array of variables that decides if we re-run this useEffect hook
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setTask(json);
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <ul>
+        <li>User id: @{task.userId}@</li>
+        <li>Id: @{task.id}@</li>
+        <li>Title: @{task.title}@</li>
+        <li>Completed: @{task.completed ? "Yes" : "No"}@</li>
+      </ul>
     </div>
   );
 }
